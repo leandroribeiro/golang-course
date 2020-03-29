c15e69e1df78061e8cc86978a0a8746a75db3ebe
Leandro Ribeiro <gitkraken@leandroribeiro.com>
2020-03-29 17:16:44 -0300

    Conteúdo até aula 18 tratamento de erros

--- sessao-2/struct-avancado/model/imovel.go
+++ sessao-2/struct-avancado/model/imovel.go
@@ -1,15 +1,31 @@
 package model
 
+import "errors"
+
 type Imovel struct {
-	X int `json:"positionX"`
-	Y int `json:"positionY"`
-	Nome string `json:"name"`
+	X     int    `json:"positionX"`
+	Y     int    `json:"positionY"`
+	Nome  string `json:"name"`
 	valor int
 }
 
+
+var ErrValorDeImovelInvalido = errors.New("O valor informado não é válido para um imovel!")
+
+var ErrValorDeImovelMuitoAlto = errors.New("O valor informado é muito alto!")
+
 //SetValor defino o valor do imóvel
-func (i *Imovel)  SetValor (valor int) {
+func (i *Imovel) SetValor(valor int) (err error) {
+	err = nil
+	if valor < 1000 {
+		err = ErrValorDeImovelInvalido
+		return
+	} else if valor > 10000{
+		err = ErrValorDeImovelMuitoAlto
+		return
+	}
 	i.valor = valor
+	return
 }
 
 //GetValor retorna o valor do imóvel
--- sessao-2/tratamento-erros/main.go
+++ sessao-2/tratamento-erros/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/leandroribeiro/go-hello-world/sessao-2/struct-avancado/model"
+)
+
 func main()  {
-	
+	casa  := model.Imovel{}
+	casa.Nome = "Casa do Zezinho"
+	casa.X = 17
+	casa.Y = 29
+	if err := casa.SetValor(50000); err !=nil {
+		fmt.Println("[main] Deu pa* meu fio!", err.Error())
+		if err == model.ErrValorDeImovelMuitoAlto {
+			fmt.Println("Está caro demais!")
+		}
+		return
+	}
+
+	fmt.Printf("Casa .... %+v\r\n", casa)
+
+	objJSON, err := json.Marshal(casa)
+	if err != nil {
+		fmt.Println("[main] Deu pa* meu fio!", err.Error())
+		return
+	}
+	fmt.Println("Casa JSON", string(objJSON))
 }
